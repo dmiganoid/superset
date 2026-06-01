5a58de1ad3f4c3ab74b0bae58d9feaeccf469d01
dmiganoid <dmiganoid@gmail.com>
2026-06-01 12:19:48 +0300

    remove_dynamic_plugin

--- superset-frontend/dynamic-rank-flow-plugin/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { RankFlowChartPlugin } from 'plugin-chart-rank-flow';
-
-console.log('[Rank Flow] bundle loaded');
-
-new RankFlowChartPlugin()
-  .configure({ key: 'plugin-chart-rank-flow' })
-  .register();
-
-console.log('[Rank Flow] plugin registered');
